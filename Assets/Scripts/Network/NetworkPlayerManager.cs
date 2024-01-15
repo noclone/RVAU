90f5e861a9b274160ba740f2ad0495baebb56f4e
noclone <noclonepvp@gmail.com>
2024-01-15 18:03:05 +0100

    runnable for 1 player

--- Assets/Scripts/Network/NetworkPlayerManager.cs
+++ Assets/Scripts/Network/NetworkPlayerManager.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class NetworkPlayerManager : MonoBehaviourPun
+public class NetworkPlayerManager : MonoBehaviourPunCallbacks
 {
     public GameObject spawnPoint1;
     public GameObject spawnPoint2;
     void Start()
     {
-        if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+        // To be removed
+        PhotonNetwork.ConnectUsingSettings();
+        /*if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
         {
             PhotonNetwork.Instantiate("Player", spawnPoint1.transform.position, Quaternion.identity);
         }
         else
         {
             PhotonNetwork.Instantiate("PlayerVR", spawnPoint2.transform.position, Quaternion.identity);
-        }
+        }*/
+    }
+
+    // To be removed
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    // To be removed
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("PlayerVR", spawnPoint2.transform.position, Quaternion.identity);
     }
 }
