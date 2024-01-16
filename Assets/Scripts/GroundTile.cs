b05aaa302203ac9e5901ce73b06ab5f9e87b96f8
Thibault Boutet <thibault.boutet@epita.fr>
2024-01-16 18:05:43 +0100

    basic obstacle generation

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    
+    void SpawnObstacle()
+    {
+        int obstacleSpawnIndex = Random.Range(1, 4);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
