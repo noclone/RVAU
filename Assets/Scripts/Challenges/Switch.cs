266227e154011123619c727545b68c4eb16b2074
Vasco Sampaio <vasco.sampaio@epita.fr>
2024-01-18 22:49:23 +0100

    Working

--- Assets/Scripts/Challenges/Switch.cs
+++ Assets/Scripts/Challenges/Switch.cs
@@ -1,61 +1,46 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
-// Assuming Light is a custom class you've created for your lights
-using Light = Assets.Scripts.Challenges.Light;
-
-public class Switch : MonoBehaviour
+public class Switch: MonoBehaviour
 {
-    public bool switchState = false; // False = Off, True = On
-    private Light[] connections; // Array of lights controlled by this switch
+    static List<int[]> combinations = new List<int[]>
+    {
+        new int[] { 0, 2, 4 },
+        new int[] { 0, 1, 3 },
+        new int[] { 0 }
+    };
+
+    public Image On;
+    public Image Off;
+    private Bulb[] bulbs;
+    private int[] combination;
 
     void Start()
     {
-        Light[] lights = FindObjectsOfType<Light>();
-
-        // Use the instance ID as a unique seed for each Switch
-        Random.InitState(GetInstanceID());
-
-        // Initialize the switches randomly with the specified combinations
-        int randomCombination = Random.Range(1, 4); // Random number between 1 and 3
-
-        switch (randomCombination)
-        {
-            case 1:
-                // Combination: Switches 1, 3, and 5 are on
-                connections = new Light[] { lights[0], lights[2], lights[4] };
-                break;
-
-            case 2:
-                // Combination: Switches 2 and 4 are on
-                connections = new Light[] { lights[1], lights[3] };
-                break;
-
-            case 3:
-                // Combination: Switch 1 is on
-                connections = new Light[] { lights[0] };
-                break;
-
-            default:
-                Debug.LogError("Invalid combination number");
-                break;
-        }
+        System.Random random = new System.Random();
+        combination = combinations[random.Next(combinations.Count)];
+        combinations.Remove(combination);
+        bulbs = GameObject.FindGameObjectsWithTag("Bulb").Select(bulb => bulb.GetComponent<Bulb>()).ToArray();
     }
 
-    // Call if player clicks on the Switch
-    void OnMouseDown()
+    public void ON()
     {
-        switchState = !switchState; // Toggle the switch state
+        Off.gameObject.SetActive(true);
+        On.gameObject.SetActive(false);
 
-        // Loop through all lights in the connections array
-        for (int i = 0; i < connections.Length; ++i)
-        {
-            // Toggle the light
-            connections[i].ToggleLight();
-        }
+        foreach (int i in combination)
+            bulbs[i].Toggle();
+    }
 
-        // Rotate the switch 180 degrees along the z-axis
-        transform.Rotate(0, 0, 180);
+    public void OFF()
+    {
+        Off.gameObject.SetActive(false);
+        On.gameObject.SetActive(true);
+
+        foreach (int i in combination)
+            bulbs[i].Toggle();
     }
 }
