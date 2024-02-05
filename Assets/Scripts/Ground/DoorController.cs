cfa3b891f073a49d326fe1766b84f206f8216c17
Theo Crepin <theo.crepin.tab@gmail.com>
2024-02-05 15:53:25 +0100

    moved PC player

--- Assets/Scripts/Ground/DoorController.cs
+++ Assets/Scripts/Ground/DoorController.cs
@@ -9,15 +9,28 @@ public class DoorController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             door.Play("DoorOpen", 0, 0.0f);
+            HideObstacles(false);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        int rnd = Random.Range(0, 2);
+        int rnd = Random.Range(0, 3);
+        rnd = 2;
         if (rnd == 0)
             GameObject.Find("GameEngine").GetComponent<GameEngine>().LoadTextMiniGame();
         else if (rnd == 1)
             GameObject.Find("GameEngine").GetComponent<GameEngine>().LoadColorMiniGame();
+        else if (rnd == 2)
+            HideObstacles(true);
+    }
+
+    private void HideObstacles(bool state)
+    {
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        foreach (GameObject obstacle in obstacles)
+        {
+            obstacle.SetActive(state);
+        }
     }
 }
